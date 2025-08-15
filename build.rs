5d6785c1250d473ee89d413818bd1db2588f8cc5
Johannes Qvarford <jq.email+github@pm.me>
2025-08-15 19:48:30 +0200

    Add icon to app.

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
     slint_build::compile("src/ui/main_ui.slint").unwrap();
+
+    // Embed icon for Windows builds
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
 }
